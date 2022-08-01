af9cd43b443adabfb5aaf38a6c2ad9d0760b01a2
Olivia Weng <oliviajweng@gmail.com>
2022-08-01 13:47:32 -0700

    fix code directory

--- script/resnet_imagenet_kube.sh
+++ script/resnet_imagenet_kube.sh
@@ -10,12 +10,13 @@ smodel_name=resnet${1}_imagenet_diraconv
 aim=${tmodel_name}_${layer}_${procedure}
 echo "teacher_name:"${tmodel_name}
 echo "student_name:"${smodel_name}
+code_dir=/imagenet-volume/pytorch/python/src/residual_distillation
 save_dir=/imagenet-volume/imagenet-training/${aim}/
 data_dir=/imagenet-volume/ILSVRC/Data/CLS-LOC/
 mode_dir=""
 seed=1
 
-CUDA_VISIBLE_DEVICES=0 python3 train_dirac.py --stage RES_NMT \
+CUDA_VISIBLE_DEVICES=0 python3 ${code_dir}/train_dirac.py --stage RES_NMT \
                        --baseline_epochs 120 \
                        --cutout_length 0 \
                        --procedure ${procedure} \
